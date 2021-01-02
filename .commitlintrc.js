e9bd272ed2bd3a5972959c5a8248fb8c110c6948
jw <bjfe@qq.com>
2021-01-02 14:46:14 +0800

    build: 增加git规范配置

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -5,9 +5,29 @@ module.exports = {
     "@commitlint/config-conventional"
   ],
   rules: {
-    'type-enum': [2, 'always', [
-      'upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert'
-    ]],
-    'subject-case': [0, 'never'],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "chore",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "type-case": [0],
+    "type-empty": [0],
+    "scope-empty": [0],
+    "scope-case": [0],
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+    "header-max-length": [0, "always", 72],
   }
 }
